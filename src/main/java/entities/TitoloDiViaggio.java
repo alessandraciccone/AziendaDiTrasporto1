7e8662fced7f5291f352a8762b05537a505bfc48
alessandraciccone <alessandraciccone@icloud.com>
2025-09-30 12:20:55 +0200

    CìommitTitolodiViaggio

--- src/main/java/entities/TitoloDiViaggio.java
+++ src/main/java/entities/TitoloDiViaggio.java
@@ -1,4 +1,71 @@
 package entities;
 
-public class TitoloDiViaggio {
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+import java.util.UUID;
+
+@Entity
+@Inheritance(strategy= InheritanceType.JOINED)
+public abstract class TitoloDiViaggio {
+    @ManyToOne
+    @JoinColumn(name ="id_punto_emissione")
+    private PuntoEmissione puntoEmissione;
+    private Double costo;
+    private String tipo;
+    private LocalDate dataEmissione;
+
+public TitoloDiViaggio(){};
+
+    public PuntoEmissione getPuntoEmissione() {
+        return puntoEmissione;
+    }
+
+    public void setPuntoEmissione(PuntoEmissione puntoEmissione) {
+        this.puntoEmissione = puntoEmissione;
+    }
+
+    public Double getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Double costo) {
+        this.costo = costo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public LocalDate getDataEmissione() {
+        return dataEmissione;
+    }
+
+    public void setDataEmissione(LocalDate dataEmissione) {
+        this.dataEmissione = dataEmissione;
+    }
+
+    @Override
+    public String toString() {
+        return "TitoloDiViaggio{" +
+                "puntoEmissione=" + puntoEmissione +
+                ", costo=" + costo +
+                ", tipo='" + tipo + '\'' +
+                ", dataEmissione=" + dataEmissione +
+                '}';
+    }
+
+    public TitoloDiViaggio(UUID idTitoloDiViaggio, PuntoEmissione puntoEmissione, Double costo, String tipo, LocalDate dataEmissione) {
+        this.puntoEmissione = puntoEmissione;
+        this.costo = costo;
+        this.tipo = tipo;
+        this.dataEmissione = dataEmissione;
+
+
+    }
 }
